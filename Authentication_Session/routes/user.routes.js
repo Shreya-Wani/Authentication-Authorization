a54c80729e175d13affe3b6e9218f022daf532ad
Shreya-Wani <shreyawani2006@gmail.com>
2025-09-05 18:11:48 +0530

    Adding Session-Based Authentication Support Using Drizzle ORM

---
+++ Authentication_Session/routes/user.routes.js
@@ -0,0 +1,82 @@
+import express from "express";
+import db from '../db/index.js';
+import {usersTable , userSessions} from '../db/schema.js';
+import { eq } from "drizzle-orm";
+import { randomBytes, createHmac } from 'crypto';
+
+const router = express.Router();
+
+router.get('/', (req,res) => {}); // Returns current logged in user
+
+router.post('/signup', async (req, res) => {
+    const {name, email, password} = req.body;
+
+    const [existingUser] = await db
+        .select({
+            email: usersTable.email,
+        })
+        .from(usersTable)
+        .where((table) => eq(table.email, email));
+
+    if (existingUser) {
+        return res
+            .status(400)
+            .json({ error: `User already with email ${email} already exists` });
+    }
+
+    const salt = randomBytes(256).toString('hex');
+    const hashPassword = createHmac('sha256', salt).update(password).digest('hex');
+
+    const [user] = await db
+        .insert(usersTable)
+        .values({
+            name,
+            email,
+            password: hashPassword,
+            salt, 
+    }).returning({id: usersTable.id});
+
+    res.status(201).json({ status: 'success', data: {userId: user.id} , message: 'User created successfully'});
+});
+
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    const [existingUser] = await db
+        .select({
+            id: usersTable.id,
+            email: usersTable.email,
+            password: usersTable.password,
+            salt: usersTable.salt,
+        })
+        .from(usersTable)
+        .where((table) => eq(table.email, email));
+
+    if (!existingUser) {
+        return res
+            .status(404)
+            .json({ error: `User with email ${email} does not exists` });
+    }
+
+    const salt = existingUser.salt;
+    const existingHash = existingUser.password;
+
+    const newHash = createHmac('sha256', salt).update(password).digest('hex');
+
+    if (newHash !== existingHash) {
+        return res.status(400).json({ error: 'Invalid password' });
+    }
+
+    //generate session for user
+    const [session] = await db
+        .insert(userSessions)
+        .values({
+            userId: existingUser.id,
+        }).returning({id: userSessions.id});
+
+
+    return res.json({status: 'success', sessionId: session.id, message: 'User logged in successfully'});
+
+});
+
+export default router;
